c9cff37c30b7f39bbf06128ea9ab079b6d2f940a
ruelasb <bruest@hotmail.com>
2017-01-19 08:07:01 -0800

    ticking clock

--- src/index.js
+++ src/index.js
@@ -3,47 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function formatDate(date) {
-	return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-	return (
-		<img className="Avatar"
-		  src={props.user.avatarUrl}
-		  alt={props.user.name}
-		/>
+function tick(){
+	const element = (
+		<div>
+			<h1>Hello, world!</h1>
+			<h2>It is {new Date().toLocaleTimeString()}</h2>
+		</div>
+	);
+	ReactDOM.render(
+		element,
+		document.getElementById('root')
 	);
 }
 
-function Comment(props) {
-	return (
-		<div className="Comment">
-		  <div className="UserInfo">
-		    <Avatar user={props.author}/>
-		    <div className="UserInfo-name">{props.author.name}</div>
-		  </div>
-		  <div className="Comment-text"> {props.text}</div>
-		  <div className="Comment-date"> {formatDate(props.date)} </div>
-		</div>
-	)
-}
-
-const comment = {
-	date: new Date(),
-	text: 'I hope you enjoy learning React!',
-	author: {
-		name: 'Hello Kitty',
-		avatarUrl: 'http://placekitten.com/g/64/64'
-	}
-};
-
-ReactDOM.render(
-  <Comment
-  	date={comment.date}
-  	text={comment.text}
-  	author={comment.author} />,
-  document.getElementById('root')
-);
+setInterval(tick, 1000);
 
 
