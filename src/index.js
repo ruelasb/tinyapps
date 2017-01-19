d9eba2392c7a1744b256f2c8290b0082bfbedc1b
ruelasb <bruest@hotmail.com>
2017-01-19 08:14:59 -0800

    ticking clock - component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function tick(){
-	const element = (
+function Clock(props){
+	return(
 		<div>
 			<h1>Hello, world!</h1>
-			<h2>It is {new Date().toLocaleTimeString()}</h2>
+		<h2>It is {props.date.toLocaleTimeString()}</h2>
 		</div>
 	);
+}
+
+function tick(){
 	ReactDOM.render(
-		element,
+		<Clock date={new Date()}/>,
 		document.getElementById('root')
-	);
+	);	
 }
 
 setInterval(tick, 1000);
