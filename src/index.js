8ea58192408fb380670024bdcdd8fadffa367f3a
ruelasb <bruest@hotmail.com>
2017-01-19 14:01:56 -0800

    Sign in/out Toggle

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+function LogoutButton (props){
+  return (<button onClick={props.onClick}>Sign Out</button>);
+}
+
+function LoginButton(props){
+ return (<button onClick={props.onClick}>Sign In</button>);
+}
+
 function UserGreeting(props){
 	return <h1>Welcome back!</h1>
 }
@@ -19,8 +27,45 @@ function Greeting(props) {
 	return <GuestGreeting />;
 }
 
+class LoginControl extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleLoginClick = this.handleLoginClick.bind(this);
+		this.handleLogoutClick = this.handleLogoutClick.bind(this);
+		this.state = {isLoggedIn: false};
+	}
+	handleLoginClick(){
+		this.setState({isLoggedIn: true});
+	}
+
+	handleLogoutClick(){
+		this.setState({isLoggedIn: false});
+	}
+
+	render(){
+		const isLoggedIn = this.state.isLoggedIn;
+
+		let button = null;
+		if(isLoggedIn){
+			button = <LogoutButton onClick={this.handleLogoutClick}/>;
+		} else {
+			button = <LoginButton onClick={this.handleLoginClick}/>;
+		}
+
+		return(
+			<div>
+				<Greeting isLoggedIn={isLoggedIn}/>
+				{button}
+			</div>
+		);
+
+	}
+
+	
+}
+
 ReactDOM.render(
-	<Greeting isLoggedIn={false}/>,
+	<LoginControl/>,
 	document.getElementById('root')
 );
 
