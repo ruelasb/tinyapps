c729cf1f4717e040e3d7629a52dbace5368d030e
ruelasb <bruest@hotmail.com>
2017-01-19 08:25:13 -0800

    clock - finished

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from './App';
 import './index.css';
 
 class Clock extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {date: new Date()};
+	}
 	render(){
 		return(
 			<div>
 				<h1>Hello, world!</h1>
-				<h2>It is {this.props.date.toLocaleTimeString()}</h2>
+				<h2>It is {this.state.date.toLocaleTimeString()}</h2>
 			</div>
 		);
 	}
@@ -16,7 +20,7 @@ class Clock extends React.Component {
 
 function tick(){
 	ReactDOM.render(
-		<Clock date={new Date()}/>,
+		<Clock />,
 		document.getElementById('root')
 	);	
 }
