b46d56916085cf7178bd6edd0df20c52cdcee02f
ruelasb <bruest@hotmail.com>
2017-01-19 18:41:32 -0800

    controlled component/textarea

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 class NameForm extends React.Component{
 	constructor(props){
 		super(props);
-		this.state = {value: ''};
+		this.state = {value: 'Please write an essay about your favorite DOM element.'};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -14,12 +14,12 @@ class NameForm extends React.Component{
 
 	handleChange(event){
 		this.setState({
-			value: event.target.value.toUpperCase()
+			value: event.target.value
 		});
 	}
 
 	handleSubmit(event){
-		alert('A name was submitted: ' + this.state.value);
+		alert('An essay was submitted: ' + this.state.value);
 		event.preventDefault();
 	}
 
@@ -28,7 +28,7 @@ class NameForm extends React.Component{
 			<form onSubmit={this.handleSubmit}>		
 			  <label>
 			  	Name:
-			  	<input type="text" value={this.state.value} onChange={this.handleChange}/>
+			  	<textarea value={this.state.value} onChange={this.handleChange} />
 			  </label>
 			  <input type="submit" value="Submit"/>
 			</form>
