96564e3c82f739574a8bb49e43dbd046e163f9af
ruelasb <bruest@hotmail.com>
2017-01-19 18:16:25 -0800

    controlled component/form

--- src/index.js
+++ src/index.js
@@ -3,21 +3,40 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-function NumberList(props){
-	const numbers = props.numbers;
-	const listItems = numbers.map((number) => 
-		<li>{number}</li>
-	);
-	return (
-		<ul>{listItems}</ul>
-	);
-};
+class NameForm extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {value: ''};
 
-const numbers = [1,2,3,4,5];
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event){
+		this.setState({
+			value: event.target.value.toUpperCase()
+		});
+	}
+
+	handleSubmit(event){
+		alert('A name was submitted: ' + this.state.value);
+		event.preventDefault();
+	}
+
+	render() {	
+		return(
+			<form onSubmit={this.handleSubmit}>		
+			  <label>
+			  	Name:
+			  	<input type="text" value={this.state.value} onChange={this.handleChange}/>
+			  </label>
+			  <input type="submit" value="Submit"/>
+			</form>
+		);
+	}
+}
 
 ReactDOM.render(
-	<NumberList numbers={numbers} />,
+	<NameForm/>,
 	document.getElementById('root')
 );
-
-
