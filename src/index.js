f2d7849c8f1559e69b1d99693bb093ab1cae87b3
ruelasb <bruest@hotmail.com>
2017-02-02 11:33:21 -0800

    ttt - loop

--- src/index.js
+++ src/index.js
@@ -5,35 +5,39 @@ import './index.css';
 
 function Square(props) {
     return (
-      <button className="square" onClick={() => props.onClick()}>
+      <button key={props.unique} className="square" onClick={() => props.onClick()}>
         {props.value}
       </button>
     );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
+  renderSquare(i, u) {
+    return <Square unique={u} key={u} value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
   }
+
+loop(){
+	var row = [];
+	var next = [];
+	for (var i = 0; i < 3; i++) {
+	  for (var y = 0; y < 3; y++) {
+	  	var z = (i*3)+y;
+		var col = 'hi';
+		next.push(this.renderSquare(z, z))
+	  }
+		row.push(<div key={i} className="board-row">{next}</div>);
+		next = [];
+	}
+	console.dir(row);
+	return row
+}
+
   render() {
+
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.loop()}
       </div>
     );
   }
