f5437f628f3bae1de625f9fd20cd47b46a1e0920
ruelasb <bruest@hotmail.com>
2017-02-07 13:16:28 -0800

    Search V1.0.0

--- src/index.js
+++ src/index.js
@@ -3,133 +3,127 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-class SearchBar extends React.Component {
-	constructor(props){
-		super(props);
-
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	handleChange() {
-		this.props.onUserInput(
-			this.filterTextInput.value,
-			this.inStockOnlyInput.checked
-		);
-	}
-
-	render() {
-		return (
-			<form>
-				<input 
-				type="text" 
-				placeholder="Search..." 
-				value={this.props.filterText} 
-				ref={(input) => this.filterTextInput = input}
-				onChange={this.handleChange}
-				/>
-				<p>
-					<input 
-					type="checkbox" 
-					checked={this.props.inStockOnly}
-					ref={(input) => this.inStockOnlyInput = input}
-					onChange={this.handleChange}
-					/>
-					{' '}
-					Only show products in stock
-				</p>
-			</form>
-		);
-	}
-}
-
-
 class ProductCategoryRow extends React.Component {
-	render(){
-		return (
-			<tr>
-				<th>{this.props.category}</th>
-			</tr>
-		)
-	}
+  render(){
+    return (
+      <tr>
+        <th colSpan="2">{this.props.category}</th>
+      </tr>
+    )
+  }
 }
 
 class ProductRow extends React.Component {
-	render(){
-		var name = this.props.product.stocked ? this.props.product.name : <span style={{color: 'red'}}>{this.props.product.name}</span>;
-		return (
-			<tr>
-				<td>{name}</td>
-				<td>{this.props.product.price}</td>
-			</tr>
-		);
-	}
+  render(){
+    var style = this.props.product.stocked ? {color: 'none'} : {color: 'red'};
+    return(
+      <tr>
+        <td style={style}>{this.props.product.name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    )
+  }
 }
 
 class ProductTable extends React.Component {
-	render() {
-		var rows = [];
-		var lastCategory = null;
-		this.props.products.forEach((product) => {
-			if(product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)){
-				return;
-			}
-			if (product.category !== lastCategory) {
-				rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-			}
-			rows.push(<ProductRow product={product} key={product.name} />);
-			lastCategory = product.category;
-		});
+  render(){
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach((product) => {
+      if(product.name.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1 || (!product.stocked && this.props.inStockOnly)){
+        return;
+      }
+      if(product.category !== lastCategory){
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name}/>);
+      lastCategory = product.category;
+    })
 
-		return (
-			<table>
-				<thead>
-						<tr>
-							<th>Name</th>
-							<th>Price</th>
-						</tr>
-				</thead>
-				<tbody>
-					{rows}
-				</tbody>
-			</table>
-		)
-	}
+    return(
+      <table>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Price</td>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(){
+    this.props.onUserInput(
+      this.filterTextInput.value,
+      this.inStockOnlyInput.checked
+    );
+  }
+
+render(){
+  return(
+    <form>
+      <input 
+      type="text" 
+      placeholder="Search..." 
+      value={this.props.fliterText}
+      ref={(input) => this.filterTextInput = input}
+      onChange={this.handleChange}/>
+      <p>
+        <input 
+          type="checkbox" 
+          checked={this.props.inStockOnly}
+          ref={(input) => this.inStockOnlyInput = input}
+          onChange={this.handleChange}/>
+          {' '}
+          Only show products in stock
+      </p>
+    </form>
+  )
+}
 }
 
 class FilterableProductTable extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			filterText: '',
-			inStockOnly: false
-		};
+  constructor(props){
+    super(props);
 
-		this.handleUserInput = this.handleUserInput.bind(this);
-	}
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    }
 
-	handleUserInput(filterText, inStockOnly){
-		this.setState({
-			filterText: filterText,
-			inStockOnly: inStockOnly
-		});
-	}
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
 
-	render() {
-		return(
-			<div>
-				<SearchBar 
-					filterText={this.state.filterText}
-					inStockOnly={this.state.inStockOnly}
-					onUserInput={this.handleUserInput}
-				/>
-				<ProductTable 
-				products={this.props.products} 
-				filterText={this.state.filterText}
-				inStockOnly={this.state.inStockOnly}
-				/>
-			</div>
-		);
-	}
+  handleUserInput(filterText, inStockOnly){
+    this.setState({
+      filterText: filterText,
+      inStockOnly: inStockOnly
+    })
+  }
+  
+  render(){
+    return(
+      <div>
+        <SearchBar 
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onUserInput={this.handleUserInput}/>
+        <ProductTable 
+          products={this.props.products} 
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}/>
+      </div>
+    )
+  }
 }
 
 var PRODUCTS = [
@@ -139,9 +133,10 @@ var PRODUCTS = [
   {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
+]
 
 ReactDOM.render(
-	<FilterableProductTable products={PRODUCTS}/>,
-	document.getElementById('root')
-);
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('root')
+)
+
